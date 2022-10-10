c9a939638dce406e9c4747d275a956dc94545b25
rolwane <rolwane@hotmail.com>
2022-10-10 11:07:12 -0300

    ref: tests

--- tests-01/src/test/java/com/bootcamp/tests01/calculator/CalculatorTest.java
+++ tests-01/src/test/java/com/bootcamp/tests01/calculator/CalculatorTest.java
@@ -1,18 +1,23 @@
 package com.bootcamp.tests01.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+    Calculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator();
+    }
 
     @Test
     @DisplayName("Testa a soma de 2 números")
     public void sum_two_numbers_and_return_double() {
         // setup
-        Calculator calculator = new Calculator();
-
         double n1 = 10;
         double n2 = 20;
         double expected = 30;
@@ -28,8 +33,6 @@ public class CalculatorTest {
     @DisplayName("Testa a divisão de 2 números positivos")
     public void divide_two_numbers_and_return_double() {
         // setup
-        Calculator calculator = new Calculator();
-
         double n1 = 30;
         double n2 = 2;
         double expect = 15;
@@ -45,8 +48,6 @@ public class CalculatorTest {
     @DisplayName("Testa a divisão de um número por 0")
     public void divide_per_zero_return_zero() {
         // setup
-        Calculator calculator = new Calculator();
-
         double n1 = 30;
         double n2 = 0;
         double expect = 0;
